22cda8248a392554cee22dcf35ee2a65eb7df4de
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2026-05-20 12:18:57 +0700

    update trade

--- src/BeeAZ/AZVanillaMobs/entity/overworld/Cat.php
+++ src/BeeAZ/AZVanillaMobs/entity/overworld/Cat.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace BeeAZ\AZVanillaMobs\entity\overworld;
 
 use BeeAZ\AZVanillaMobs\entity\Animal;
+<<<<<<< HEAD
+use pocketmine\item\VanillaItems;
+=======
+>>>>>>> 018dd6d048cbb1352c9a33566660fd435ec30cfb
 
 class Cat extends Animal {
     public static function getNetworkTypeId(): string {
@@ -12,8 +16,12 @@ class Cat extends Animal {
     }
 
     public function isBreedingItem(\pocketmine\item\Item $item): bool {
+<<<<<<< HEAD
+        return $item->equals(VanillaItems::RAW_FISH(), true, false) || $item->equals(VanillaItems::RAW_SALMON(), true, false);
+=======
         $typeId = $item->getTypeId();
         return $typeId === \pocketmine\item\VanillaItems::RAW_COD()->getTypeId() ||
                $typeId === \pocketmine\item\VanillaItems::RAW_SALMON()->getTypeId();
+>>>>>>> 018dd6d048cbb1352c9a33566660fd435ec30cfb
     }
 }
